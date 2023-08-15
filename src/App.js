4c60da4bb293082fc81d340f138fd69090a9ba29
jmoseka <jamylam3@gmail.com>
2023-08-15 20:54:54 +0300

    add background gradient color on the main page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Prediction from './components/myprediction';
 // import {ReactComponent as Virgo} from './SVG/virgo.svg';
 
 
 function App() {
   return (
-    <div className="App">
-      <h1 className='text-3xl font-bold underline bg-slate-600'>hey</h1>
+    <div className="App h-screen w-screen">
+      <div className='max-w-[1800px] mx-auto h-full'>
+        <div className='body-gradient-bg h-full' >
+          <Prediction />
+        </div>
+
+      </div>
     </div>
   );
 }
---
+++ src/components/myprediction.js
@@ -0,0 +1,14 @@
+// import {ReactComponent as Virgo} from './SVG/virgo.svg';
+
+
+function Prediction() {
+  return (
+    <div className="container mx-auto h-full w-full">
+
+        
+      
+    </div>
+  );
+}
+
+export default Prediction;
