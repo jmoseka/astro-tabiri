6c80d36de0c151053d4ede5fd35e6e8d4f26cb17
jmoseka <jamylam3@gmail.com>
2023-08-15 15:46:53 +0300

    add colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,25 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  
   theme: {
-    extend: {},
-  },
-  plugins: [],
+    extend: {
+      fontFamily: {
+        sans: ['Barlow Semi Condensed', 'sans-serif'],
+      },
+
+      gridTemplateRows: {
+        '10': 'repeat(10, minmax(0, 1fr))',
+      },
+    
+      colors: {
+        strongBlue: '#010413',
+        blue: '#041925',
+        yellow: '#FCDA54',
+        lightYellow: '#DFDAC7',
+       
+      },
+    },
+  }
+
 }
