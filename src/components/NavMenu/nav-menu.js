ad8df719a7358c17490f32b695cd6b4e1ff65462
jmoseka <jamylam3@gmail.com>
2023-08-24 22:00:53 +0300

    add nav links

--- src/components/NavMenu/nav-menu.js
+++ src/components/NavMenu/nav-menu.js
@@ -3,15 +3,22 @@ import './nav-menu.css'
 function NavMenu() {
 
     return (
-        <div className="navmenu absolute bg-blue h-32 w-full opacity-80 z-10">
-            <div className='container h-full w-full mx-auto flex'>
-                <button className='my-auto relative humberger' id="menu-btn" type="button">
+        <header className="header absolute  h-32 w-full opacity-80 z-10">
+            <div className='container h-full w-full mx-auto flex items-center'>
+                <div className='flex gap-32'>
+                <button className='relative humberger' id="menu-btn" type="button">
                     <span className="top"></span>
                     <span className="middle "></span>
                     <span className="bottom"></span>
                 </button>
+
+                <nav className='nav-links flex gap-28 text-lightYellow text-xl'>
+                    <a href='./j'>Daily Horoscope</a>
+                    <a href='./m'>Zodiac</a>
+                </nav>
+                </div>
             </div>
-        </div>
+        </header>
     )
 }
 
