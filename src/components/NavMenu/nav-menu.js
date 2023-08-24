0b0fd652649fd0927af1abee495e53363de738f8
jmoseka <jamylam3@gmail.com>
2023-08-24 22:26:33 +0300

    add zodiac list

--- src/components/NavMenu/nav-menu.js
+++ src/components/NavMenu/nav-menu.js
@@ -3,19 +3,45 @@ import './nav-menu.css'
 function NavMenu() {
 
     return (
-        <header className="header absolute  h-32 w-full opacity-80 z-10">
+        <header className="header absolute  h-28 w-full opacity-80 z-10">
             <div className='container h-full w-full mx-auto flex items-center'>
                 <div className='flex gap-32'>
-                <button className='relative humberger' id="menu-btn" type="button">
-                    <span className="top"></span>
-                    <span className="middle "></span>
-                    <span className="bottom"></span>
-                </button>
+                    <button className='relative humberger' id="menu-btn" type="button">
+                        <span className="top"></span>
+                        <span className="middle "></span>
+                        <span className="bottom"></span>
+                    </button>
 
-                <nav className='nav-links flex gap-28 text-lightYellow text-xl'>
-                    <a href='./j'>Daily Horoscope</a>
-                    <a href='./m'>Zodiac</a>
-                </nav>
+                    <nav className='nav-links flex gap-28 text-lightYellow text-xl'>
+                        <a href='./j'>Daily Horoscope</a>
+                        <div>
+                            <a href='./m'>Zodiac</a>
+
+                            <div className='absolute rounded-lg py-2 bg-white text-base text-strongBlue flex'>
+                                <ul className='px-4'>
+                                    <li>Pisces</li>
+                                    <li>Aries</li>
+                                    <li>Capricorn</li>
+                                    <li>Gemini</li>
+                                    <li>Libra</li>
+                                    <li>Taurus</li>
+
+                                </ul>
+
+                                <ul className='px-4'>
+                                    <li>Cancer</li>
+                                    <li>Virgo</li>
+                                    <li>Scorpio</li>
+                                    <li>Leo</li>
+                                    <li>Sagittarius</li>
+                                    <li>Aquarius</li>
+                                </ul>
+                            </div>
+
+
+                        </div>
+
+                    </nav>
                 </div>
             </div>
         </header>
