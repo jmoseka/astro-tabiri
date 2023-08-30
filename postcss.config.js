162680740bc738461ae35eb70246159ea9a676d5
jmoseka <jamylam3@gmail.com>
2023-08-30 17:19:48 +0300

    install postcss-nested plugin

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,8 @@
 module.exports = {
-    style: {
-      postcss: {
-        loaderOptions: (postcssLoaderOptions) => {
-          postcssLoaderOptions.postcssOptions.plugins = [
-            require('tailwindcss/nesting'),
-            require('tailwindcss'),
-            require('postcss-mixins'),
-            'postcss-flexbugs-fixes',
-            [
-              'postcss-preset-env',
-              {
-                autoprefixer: {
-                  flexbox: 'no-2009',
-                },
-                stage: 0,
-              },
-            ],
-          ]
-  
-          return postcssLoaderOptions
-        },
-      },
-    },
-  }
+    plugins: [
+      require('postcss-import'),
+      require('@tailwindcss/nesting'),
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ]
+}
