65d85b70e47ad74b99c84b424428a6d9f645dadb
jmoseka <jamylam3@gmail.com>
2023-08-30 18:00:34 +0300

    add to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,6 +28,11 @@ module.exports = {
     },
   },
 
-
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ]
 
 }
