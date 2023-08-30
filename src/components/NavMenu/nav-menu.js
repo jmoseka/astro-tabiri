34e8ce9242ff046eca4e42c7bba200e61d712707
jmoseka <jamylam3@gmail.com>
2023-08-30 18:55:57 +0300

    disable

--- src/components/NavMenu/nav-menu.js
+++ src/components/NavMenu/nav-menu.js
@@ -19,6 +19,7 @@ function NavMenu({ updateOverlayState, isOverlayClick }) {
             updateOverlayState(!isOverlayOpen);
         }
 
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [isOverlayClick]);
 
     const dropMenu = () => {
