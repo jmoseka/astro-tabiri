9cc69b93578c8f66c998619c2358476b7155e668
jmoseka <jamylam3@gmail.com>
2023-08-30 15:25:44 +0300

    fix nested css issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,7 +28,10 @@ module.exports = {
   },
 
   plugins: [
+    require('postcss-import'),
+    require('tailwindcss/nesting')(require('postcss-nesting')),
     require('autoprefixer'),
+    require('tailwindcss'),
   ]
 
 }
