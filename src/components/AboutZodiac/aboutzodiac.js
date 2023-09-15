c4eb46afe1718d0e541738186e3918c982502ef2
jmoseka <jamylam3@gmail.com>
2023-09-16 00:05:55 +0300

    content layout for mobile screen

--- src/components/AboutZodiac/aboutzodiac.js
+++ src/components/AboutZodiac/aboutzodiac.js
@@ -58,7 +58,7 @@ const AboutZodiac = () => {
 
 
     return (
-        <div ref={containerRef} className="h-fit w-full mx-auto bg-lightMainColor py-6 dark:bg-darkMainColor">
+        <div ref={containerRef} className="h-fit w-full mx-auto bg-lightMainColor  dark:bg-darkMainColor">
             <nav className="p-2">
                 <button type="button " onClick={() => toggleDarkLightMode()} className="modeBtn fixed text-[1.6rem] left-[4rem] cursor-pointer">
                     {
@@ -78,7 +78,7 @@ const AboutZodiac = () => {
 
 
 
-                    <div className="flex flex-col gap-6 sticky top-20 px-2 h-fit rounded-lg sidebar-nav
+                    <div className="flex flex-col gap-6 sticky top-20 px-2 py-4 h-fit rounded-lg sidebar-nav
                       bg-lightSecondMainColor dark:bg-darkSecondMainColor
                       dark:text-lightYellow text-darkTextGold">
 
@@ -131,19 +131,23 @@ const AboutZodiac = () => {
                     </div>
 
 
-                    <div className="flex flex-col gap-8">
+                    <div className="flex flex-col gap-8 w-full text-darkTextGold dark:text-lightYellow ">
+
+
+
+                        
                         <div className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor ">
                             <h1 className="text-2xl capitalize">{name}</h1>
                             <p>Strengths: {strengths}</p>
                             <p>Weaknesses: {weaknesses}</p>
                         </div>
-                        <div id="idabout" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">About</h2> <p>{about}</p></div>
-                        <div id="idcareer" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">Career</h2><p>{career}</p></div>
-                        <div id="idlove" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">Love</h2><p>{love}</p></div>
-                        <div id="idhealth" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">Health</h2><p>{health}</p></div>
-                        <div id="idman" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">Man</h2><p>{man}</p></div>
-                        <div id="idwoman" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">Woman</h2>{woman}</div>
-                        <div id="idrelationship" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="text-2xl capitalize ">relationship</h2><p>{relationship}</p></div>
+                        <div id="idabout" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor "><h2 className="zodiac-about-title">About</h2> <p className="zodiac-p text-darkTextGold dark:text-lightYellow {woman}">{about}</p></div>
+                        <div id="idcareer" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="zodiac-about-title">Career</h2><p className="zodiac-p {woman}">{career}</p></div>
+                        <div id="idlove" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="zodiac-about-title">Love</h2><p className="zodiac-p {woman}">{love}</p></div>
+                        <div id="idhealth" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="zodiac-about-title">Health</h2><p className="zodiac-p {woman}">{health}</p></div>
+                        <div id="idman" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="zodiac-about-title">Man</h2><p className="zodiac-p {woman}">{man}</p></div>
+                        <div id="idwoman" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="zodiac-about-title">Woman</h2><p className="zodiac-p {woman}">{woman}</p></div>
+                        <div id="idrelationship" className="zodiac-card bg-lightSecondMainColor dark:bg-darkSecondMainColor"><h2 className="zodiac-about-title">relationship</h2><p className="zodiac-p {woman}">{relationship}</p></div>
                     </div>
                 </div>
             </div>
