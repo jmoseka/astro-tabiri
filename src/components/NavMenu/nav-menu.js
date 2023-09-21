e0b2f75d63f951dd07a66dc32e931f588799854f
jmoseka <jamylam3@gmail.com>
2023-09-21 14:24:15 +0300

    remove unecessary div

--- src/components/NavMenu/nav-menu.js
+++ src/components/NavMenu/nav-menu.js
@@ -37,10 +37,10 @@ function NavMenu() {
         <header className={`absolute  text-textLightGold header w-full 
          ${isHeaderPresent ? 'header-present' : ''} flex `}>
 
-            <nav className=' w-[92%] h-fit text-[.8rem] nav-menu flex justify-end gap-24  '>
+            <nav className=' w-[98%] md:w-[92%]  h-fit text-[.8rem] nav-menu flex justify-end gap-24  '>
 
             <button onClick={() => dropMenu()} typeof='button' id='nav-btn'
-                    className='nav-button order-last cursor-pointer z-30 border border-3 h-fit flex items-center justify-center'>
+                    className='nav-button order-last cursor-pointer z-30 h-fit flex items-center justify-center'>
                     <div className={`flex cursor-pointer flex-col  items-center w-[3.7rem] h-[3.7rem] `}>
                         <div className={`relative translate-y-4 humberger ${isOpen ? 'open' : ''}`} id="menu-btn">
                             <span className={`top bg-darkGold`}></span>
@@ -59,19 +59,19 @@ function NavMenu() {
                         <div className={`relative mx-auto}`}>
                             {/* zodiac sign button labels */}
 
-                            <button onClick={() => displayZodiac()} className='border py-1 px-6 w-full flex gap-1 justify-center items-center hover:text-yellow '>
+                            <button onClick={() => displayZodiac()} className='py-1 px-6 w-full flex gap-1 justify-center items-center hover:text-yellow '>
                                 <p className='uppercase zod-link '>Zodiac signs</p>
                                 {isZodiacListOpen ? <MdOutlineKeyboardArrowUp className='text-[1.4rem]' /> :
                                     <MdOutlineKeyboardArrowDown className='text-[1.4rem]' />}
 
                             </button>
 
-                            <div className={` translate-y-3`}>
+                            <div className={` translate-y-2`}>
 
                                 <div>
                                     {
                                         isZodiacListOpen ?
-                                            <div className='bg-lightBlue border rounded-md text-[.9rem]'>
+                                            <div className='bg-lightBlue text-[.9rem]'>
                                                 <ZodiacSignList />
                                             </div>
 
