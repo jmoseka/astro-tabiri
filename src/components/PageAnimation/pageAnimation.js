04fae12ddb8dedce3daf3fdf37bdc0ac1b9736bc
jmoseka <jamylam3@gmail.com>
2023-10-01 21:29:28 +0300

    add twinkle star animation

--- src/components/PageAnimation/pageAnimation.js
+++ src/components/PageAnimation/pageAnimation.js
@@ -1,25 +1,55 @@
 import './pageAnimation.css';
-
 const PageAnimation = () => {
-    const divElements = [];
+  const divElements = [];
+  const movingStarsElement = [];
 
-    for (let i = 0; i < 100; i++) {
+  const randomNumber = () => {
+    let result = Math.floor(Math.random() * 101);
+    result = result.toString()
+    return result;
+  }
+
+  const movingSeconds = () => {
+    const result = Math.floor(Math.random() * 9);
+    return result.toString();
+  }
+
+
+  const twinkleStar = () => {
+    const twinkleSeconds = () => {
+      const result = (Math.floor(Math.random() * 41) + 10) / 10;
+      return result.toString();
+    }
+
+
+    for (let i = 0; i < 20; i++) {
+      const myStyles = {
+        top: `${randomNumber()}%`,
+        left: `${randomNumber()}%`,
+        animation: `twinkle ${twinkleSeconds()}s linear infinite`,
+      };
+
       divElements.push(
-        <div key={i}>
-          Div {i + 1}
+        <div style={myStyles} className={`star absolute`} key={i}>
         </div>
       );
     }
+  }
 
-    return (
-        <div className="absolute h-screen w-full ">
-            <div class="starry-night">
-               <div>{divElements}</div>
-            </div>
+  twinkleStar();
+  movingStars();
 
-        </div>
-    )
 
+
+  return (
+    <div className="absolute h-screen w-full">
+      <div className="starry-night  h-screen w-full relative">
+        {divElements}
+        {movingStarsElement}
+
+      </div>
+    </div>
+  )
 }
 
-export default PageAnimation;
+export default PageAnimation
