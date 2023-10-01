4235c2a953111fce307e9edb998ee9a6d91f33d8
jmoseka <jamylam3@gmail.com>
2023-10-01 23:55:17 +0300

    finalize animation

--- src/components/PageAnimation/pageAnimation.js
+++ src/components/PageAnimation/pageAnimation.js
@@ -1,7 +1,8 @@
 import './pageAnimation.css';
 const PageAnimation = () => {
   const divElements = [];
-  const movingStarsElement = [];
+  const movingSmallStars = [];
+  const movingAverageStars = [];
 
   const randomNumber = () => {
     let result = Math.floor(Math.random() * 101);
@@ -10,10 +11,36 @@ const PageAnimation = () => {
   }
 
   const movingSeconds = () => {
-    const result = Math.floor(Math.random() * 9);
+    const result = Math.floor(Math.random() * 6);
     return result.toString();
   }
 
+
+
+
+  const movingStars = () => {
+    for (let i = 0; i < 30; i++) {
+      const animationDelay = `${Math.random() * 25}s`;
+      const myStyles = {
+        top: `${randomNumber()}%`,
+        left: `${randomNumber()}%`,
+        animation: `animMovingStar 15s linear infinite`,
+        animationDelay: animationDelay,
+
+      };
+
+      // Add random animation delays
+
+      movingSmallStars.push(
+        <div
+          className={`w-[1px] h-[1px] bg-white absolute`}
+          style={myStyles}
+          key={i}
+        ></div>
+      );
+    }
+
+  }
 
   const twinkleStar = () => {
     const twinkleSeconds = () => {
@@ -22,7 +49,7 @@ const PageAnimation = () => {
     }
 
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 10; i++) {
       const myStyles = {
         top: `${randomNumber()}%`,
         left: `${randomNumber()}%`,
@@ -41,11 +68,14 @@ const PageAnimation = () => {
 
 
 
+
   return (
     <div className="absolute h-screen w-full">
       <div className="starry-night  h-screen w-full relative">
         {divElements}
-        {movingStarsElement}
+        {movingSmallStars}
+        <div className='h-5 w-5 bg-orange-800 comet'></div>
+ 
 
       </div>
     </div>
